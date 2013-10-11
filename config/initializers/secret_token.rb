fc0c60a1938ae1bb451a23421bc1e0fdb5b74c30
pyoracle <zhoujj519@hotmail.com>
2013-10-10 20:04:40 -0400

    Generated Rails 3 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'fb28d0ac13775b90d76fccd9158207d902b021bcc0e3cb46f87d644c455a7f0dbf2f732a00a7b1c45d9ad187d0a9fd9b16b2611de48a1ffc7f6716a62bf1a564'
